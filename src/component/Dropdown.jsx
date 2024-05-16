602ae7930da02dc732e3beea5f6041843663aa24
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-05-16 20:59:36 +0800

    Update

---
+++ src/component/Dropdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Dropdown({btnName, onClick}) {
+  return (
+    <div className='grid place-items-center py-2 px-5 hover:bg-white cursor-pointer rounded-lg'>
+        <button
+            onClick={onClick}
+        >{btnName}</button>
+    </div>
+  )
+}
+
+export default Dropdown
